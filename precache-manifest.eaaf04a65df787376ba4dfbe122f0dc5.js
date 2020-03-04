11af3e57ea2c78c10ff1bdefdba5f8a5ef5cad08
Curtis Noonan <curtis.noonan@gmail.com>
2020-03-03 16:41:23 -0800

    Updates

---
+++ precache-manifest.eaaf04a65df787376ba4dfbe122f0dc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd98297ff1032f06b3b4ec6b6c3e1ac1",
+    "url": "/fruits-list/index.html"
+  },
+  {
+    "revision": "cc6a5aeb1623ae5a0a30",
+    "url": "/fruits-list/static/css/main.3454a4b1.chunk.css"
+  },
+  {
+    "revision": "5ef378234866a0faa395",
+    "url": "/fruits-list/static/js/2.5bae18f8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/fruits-list/static/js/2.5bae18f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc6a5aeb1623ae5a0a30",
+    "url": "/fruits-list/static/js/main.5eb6f258.chunk.js"
+  },
+  {
+    "revision": "629d63664b5e51c4da54",
+    "url": "/fruits-list/static/js/runtime-main.fe767335.js"
+  }
+]);
